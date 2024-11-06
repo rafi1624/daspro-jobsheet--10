97634343f6477e5361cc4295164c6735f2433010
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2024-11-06 21:46:21 +0700

    Percobaan 1 dan Jawaban Pertanyaanya

---
+++ Bioskop23.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Bioskop23 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         String [][] penonton = new String[4][2];
+
+         penonton [0][0] = "Amin";
+         penonton [0][1] = "Bena";
+         penonton [1][0] = "Candra";
+         penonton [1][1] = "Dela";
+         penonton [2][0] = "Eka";
+         penonton [2][1] = "Farhan";
+         penonton [3][0] = "Gisel";
+         penonton [3][1] = "Hana";
+
+         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+         System.out.println(penonton.length);
+
+         System.out.println(penonton.length);
+
+         for (String[] barisPenonton : penonton){
+            System.out.println("Panjang Baris: " + barisPenonton.length);
+         }
+         
+         System.out.println("Penonton Pada Baris Ke-3: ");
+
+         for (String i : penonton[2]){
+            System.out.println(i);
+         }
+         
+         for (int i = 0; i < penonton.length; i++){
+            System.out.println("Penonton Pada Baris Ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
+
+         }
+    }
+}
